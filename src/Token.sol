a6f5f7d4b25237e046d86650647dd36b243f5475
Minh Pham <minh@gravitygroup.io>
2023-07-18 10:39:54 +0800

    init project structure

---
+++ src/Token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+contract GRVTExchange {
+    struct Account {
+        uint id;
+    }
+
+    struct SubAccount {
+        uint id;
+    }
+}
