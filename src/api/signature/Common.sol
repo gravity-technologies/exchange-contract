f725de06e7724d5a50e86a177fd03f9a7bb8a626
Minh Pham <minh@gravitygroup.io>
2023-08-01 14:20:25 +0800

    chore: formatting

--- src/api/signature/Common.sol
+++ src/api/signature/Common.sol
@@ -13,15 +13,8 @@ struct EIP712Domain {
 
 // prettier-ignore
 bytes32 constant eip712domainTypehash = keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
-bytes32 constant domainHash = keccak256(
-  abi.encode(
-    eip712domainTypehash,
-    keccak256(bytes("GRVTEx")),
-    keccak256(bytes("0")), // version
-    0, //chainID
-    address(0) // verifyingContract
-  )
-);
+// prettier-ignore
+bytes32 constant domainHash = keccak256( abi.encode(eip712domainTypehash, keccak256(bytes("GRVTEx")), keccak256(bytes("0")), 0, address(0)));
 
 function verify(
   mapping(bytes32 => bool) storage isExecuted,
@@ -29,8 +22,8 @@ function verify(
   bytes32 payloadHash,
   Signature calldata sig
 ) {
-  require(sig.expiration > 0 && sig.expiration > timestamp, "signature expired");
-  require(!isExecuted[payloadHash], "transaction is not unique");
+  require(sig.expiration > 0 && sig.expiration > timestamp, "expired");
+  require(!isExecuted[payloadHash], "invalid transaction");
   isExecuted[payloadHash] = true;
   bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainHash, payloadHash));
   (address signerAddr, ECDSA.RecoverError err) = ECDSA.tryRecover(digest, sig.v, sig.r, sig.s);
