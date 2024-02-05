191ea802b4f3d8a733a6be53d8933b44736349f0
alaukiknpant <alaukikn.pant@gmail.com>
2024-02-05 13:16:05 +0800

    Make it work

--- deploy/utils.ts
+++ deploy/utils.ts
@@ -173,3 +173,88 @@ export const LOCAL_RICH_WALLETS = [
     privateKey: "0x3eb15da85647edd9a1159a4a13b9e7c56877c4eb33f614546d4db06a51868b1c",
   },
 ]
+
+export const deployContractUpgradable = async (
+  contractArtifactName: string,
+  initializationVariables?: any[],
+  options?: DeployContractOptions
+) => {
+  const contractName = contractArtifactName
+  console.log("Deploying " + contractName + "...")
+  const log = (message: string) => {
+    if (!options?.silent) console.log(message)
+  }
+
+  log(`\nStarting deployment process of "${contractArtifactName}"...`)
+
+  const zkWallet = options?.wallet ?? getWallet()
+  const deployer = new Deployer(hre, zkWallet)
+  const contract = await deployer.loadArtifact(contractArtifactName).catch((error) => {
+    if (error?.message?.includes(`Artifact for contract "${contractArtifactName}" not found.`)) {
+      console.error(error.message)
+      throw `⛔️ Please make sure you have compiled your contracts or specified the correct contract name!`
+    } else {
+      throw error
+    }
+  })
+
+  const chainId = hre.network.config.chainId
+  // only chainID 324 and 280 are supported to estimate gas for proxies
+  //   if (chainId != undefined && (chainId == 324 || chainId == 280)) {
+  //     // Estimate contract deployment fee
+  //     const deploymentFee = await hre.zkUpgrades.estimation.estimateGasProxy(deployer, contract, [], {
+  //       kind: "transparent",
+  //     })
+  //     log(`Estimated deployment cost: ${ethers.formatEther(deploymentFee)} ETH`)
+
+  //     // Check if the wallet has enough balance
+  //     await verifyEnoughBalance(zkWallet, deploymentFee)
+  //   }
+
+  // Deploy the contract to zkSync via proxy
+  const proxiedContract = await hre.zkUpgrades.deployProxy(deployer.zkWallet, contract, [initializationVariables], {
+    initializer: "initialize",
+  })
+  await proxiedContract.deployed()
+
+  // const proxiedContract = await deployer.deploy(contract, constructorArguments)
+  const address = await proxiedContract.address
+  const constructorArgs = proxiedContract.interface.encodeDeploy(initializationVariables)
+  const fullContractSource = `${contract.sourceName}:${contract.contractName}`
+  console.log(contractArtifactName + " deployed to:", address)
+
+  // Display contract deployment info
+  log(`\n"${contractArtifactName}" was successfully deployed:`)
+  log(` - Contract address: ${address}`)
+  log(` - Contract source: ${fullContractSource}`)
+  log(` - Encoded constructor arguments: ${constructorArgs}\n`)
+
+  return proxiedContract
+}
+
+export const upgradeTransparentUpgradeableProxy = async (
+  upgradeContractArtifactName: string,
+  proxyAddress: string,
+  options?: DeployContractOptions
+) => {
+  const contractName = upgradeContractArtifactName
+  console.log("Deploying " + contractName + "...")
+  const log = (message: string) => {
+    if (!options?.silent) console.log(message)
+  }
+
+  log(`\nStarting upgrade process of "${upgradeContractArtifactName}"...`)
+
+  const zkWallet = options?.wallet ?? getWallet()
+  const deployer = new Deployer(hre, zkWallet)
+  const contractV2 = await deployer.loadArtifact(upgradeContractArtifactName).catch((error) => {
+    if (error?.message?.includes(`Artifact for contract "${upgradeContractArtifactName}" not found.`)) {
+      console.error(error.message)
+      throw `⛔️ Please make sure you have compiled your contracts or specified the correct contract name!`
+    } else {
+      throw error
+    }
+  })
+
+  await hre.zkUpgrades.upgradeProxy(deployer.zkWallet, proxyAddress, contractV2)
+}
