094c0ba0ea374d02e1da6d26cc7fb3164a4b7fb9
alaukiknpant <alaukikn.pant@gmail.com>
2024-02-07 18:10:44 +0545

    Add sample foundry test

---
+++ test-foundry/SampleTest.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "../lib/forge-std/src/Test.sol";
+
+contract SampleTest is Test {
+  function testSample() public {
+    assertEq(true, true);
+  }
+}
