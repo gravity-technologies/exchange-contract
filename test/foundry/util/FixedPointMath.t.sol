6e7dfdf396971f76f7a02b1c0539bd84b908238e
alaukiknpant <alaukikn.pant@gmail.com>
2024-02-08 12:30:22 +0545

    Complete FixedPointMath testing initial round

--- test/foundry/util/FixedPointMath.t.sol
+++ test/foundry/util/FixedPointMath.t.sol
@@ -7,7 +7,7 @@ import "../../../contracts/exchange/util/FixedPointMath.sol";
 import "../../../contracts/exchange/types/DataStructure.sol";
 
 contract FixedPointMathTest is Test {
-  function testAdd() public {
+  function testAddEqualDecimals() public {
     BI memory a = BI(1, 18);
     BI memory b = BI(1, 18);
     BI memory c = FixedPointMath.add(a, b);
@@ -15,7 +15,23 @@ contract FixedPointMathTest is Test {
     assertEq(c.dec, 18);
   }
 
-  function testSub() public {
+  function testAddFirstOperandGreaterDecimals() public {
+    BI memory a = BI(1, 18);
+    BI memory b = BI(1, 15);
+    BI memory c = FixedPointMath.add(a, b);
+    assertEq(c.val, 1001);
+    assertEq(c.dec, 18);
+  }
+
+  function testAddSecondOperandGreaterDecimals() public {
+    BI memory a = BI(1, 15);
+    BI memory b = BI(1, 18);
+    BI memory c = FixedPointMath.add(a, b);
+    assertEq(c.val, 1001);
+    assertEq(c.dec, 18);
+  }
+
+  function testSubEqualDecimals() public {
     BI memory a = BI(1, 18);
     BI memory b = BI(1, 18);
     BI memory c = FixedPointMath.sub(a, b);
@@ -23,7 +39,23 @@ contract FixedPointMathTest is Test {
     assertEq(c.dec, 18);
   }
 
-  function testMul() public {
+  function testSubFirstOperandGreaterDecimals() public {
+    BI memory a = BI(1, 18);
+    BI memory b = BI(1, 15);
+    BI memory c = FixedPointMath.sub(a, b);
+    assertEq(c.val, -999);
+    assertEq(c.dec, 18);
+  }
+
+  function testSubSecondOperandGreaterDecimals() public {
+    BI memory a = BI(1, 15);
+    BI memory b = BI(1, 18);
+    BI memory c = FixedPointMath.sub(a, b);
+    assertEq(c.val, 999);
+    assertEq(c.dec, 18);
+  }
+
+  function testMulEqualDecimals() public {
     BI memory a = BI(1, 18);
     BI memory b = BI(1, 18);
     BI memory c = FixedPointMath.mul(a, b);
@@ -31,11 +63,41 @@ contract FixedPointMathTest is Test {
     assertEq(c.dec, 36);
   }
 
+  function testMulFirstOperandGreaterDecimals() public {
+    BI memory a = BI(1, 18);
+    BI memory b = BI(1, 15);
+    BI memory c = FixedPointMath.mul(a, b);
+    assertEq(c.val, 1);
+    assertEq(c.dec, 33);
+  }
+
+  function testMulSecondOperandGreaterDecimals() public {
+    BI memory a = BI(1, 18);
+    BI memory b = BI(1, 15);
+    BI memory c = FixedPointMath.mul(a, b);
+    assertEq(c.val, 1);
+    assertEq(c.dec, 33);
+  }
+
   function testDiv() public {
     BI memory a = BI(1, 18);
     BI memory b = BI(1, 18);
     BI memory c = FixedPointMath.div(a, b);
-    assertEq(c.val, 1);
+    assertEq(c.val, 1000000000000000000);
     assertEq(c.dec, 18);
+  }
+
+  function testCmp() public {
+    BI memory a = BI(1, 18);
+    BI memory b = BI(1, 18);
+    int256 c = FixedPointMath.cmp(a, b);
+    assertEq(c, 0);
+
+    BI memory d = BI(1, 18);
+    BI memory e = BI(2, 18);
+    int256 f = FixedPointMath.cmp(d, e);
+    int256 g = FixedPointMath.cmp(e, d);
+    assertEq(f, -1);
+    assertEq(g, 1);
   }
 }
