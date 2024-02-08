d34757ed06b31efcb64d61671c3f9ebc932aae2d
alaukiknpant <alaukikn.pant@gmail.com>
2024-02-08 15:32:24 +0545

    Checkmark

---
+++ test/foundry/types/Types.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+struct Users {
+  address payable gravity;
+  address payable walletOne;
+  address payable walletTwo;
+  address payable walletThree;
+  address payable walletFour;
+  address payable walletFive;
+  address payable walletSix;
+  address payable walletSeven;
+}
