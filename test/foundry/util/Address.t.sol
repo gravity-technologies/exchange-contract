13a9f90e406472b973a955b2e424545b1151cd36
alaukiknpant <alaukikn.pant@gmail.com>
2024-02-08 11:18:00 +0545

    Addresses.sol

--- test/foundry/util/Address.t.sol
+++ test/foundry/util/Address.t.sol
@@ -3,10 +3,13 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "../../../lib/forge-std/src/Test.sol";
 import "../../../contracts/exchange/util/Address.sol";
+import "../../../contracts/exchange/types/DataStructure.sol";
 
 contract AddressTest is Test {
   address[] public addresses;
-  mapping(address => uint64) signers;
+  mapping(address => uint64) mockSigners;
+  mapping(address => uint64) accountSigners;
+  mapping(address => uint64) subAccountSigners;
 
   function testAddressExists() public pure {
     address[] memory arr = new address[](3);
@@ -29,15 +32,34 @@ contract AddressTest is Test {
     assert(addresses.length == 4);
   }
 
-  function testSignerHasPerm() public {
+  function testSignerHasPermMock() public {
     address signerAddress = address(0x123);
     uint64 perm = 1; // Example permission
-
-    // Add signer with permission
-    signers[signerAddress] = perm;
-
-    // Test the function
-    bool hasPerm = signerHasPerm(signers, signerAddress, perm);
+    mockSigners[signerAddress] = perm;
+    bool hasPerm = signerHasPerm(mockSigners, signerAddress, perm);
     assert(hasPerm);
+  }
+
+  function testSignerHasPermAccount() public {
+    address signerAddress = address(0x123);
+    accountSigners[signerAddress] = AccountPermInternalTransfer | AccountPermExternalTransfer | AccountPermWithdraw;
+    assert(signerHasPerm(accountSigners, signerAddress, AccountPermInternalTransfer));
+    assert(signerHasPerm(accountSigners, signerAddress, AccountPermExternalTransfer));
+    assert(signerHasPerm(accountSigners, signerAddress, AccountPermWithdraw));
+    assert(!signerHasPerm(accountSigners, signerAddress, AccountPermAdmin));
+  }
+
+  function testSignerHasPermSubAccount() public {
+    address signerAddress = address(0x123);
+    accountSigners[signerAddress] = SubAccountPermDeposit | SubAccountPermWithdrawal | SubAccountPermTrade;
+    assert(signerHasPerm(accountSigners, signerAddress, SubAccountPermDeposit));
+    assert(signerHasPerm(accountSigners, signerAddress, SubAccountPermWithdrawal));
+    assert(signerHasPerm(accountSigners, signerAddress, SubAccountPermTrade));
+    assert(!signerHasPerm(accountSigners, signerAddress, SubAccountPermAdmin));
+    assert(!signerHasPerm(accountSigners, signerAddress, SubAccountPermTransfer));
+    assert(!signerHasPerm(accountSigners, signerAddress, SubAccountPermAddSigner));
+    assert(!signerHasPerm(accountSigners, signerAddress, SubAccountPermRemoveSigner));
+    assert(!signerHasPerm(accountSigners, signerAddress, SubAccountPermUpdateSignerPermission));
+    assert(!signerHasPerm(accountSigners, signerAddress, SubAccountPermChangeMarginType));
   }
 }
