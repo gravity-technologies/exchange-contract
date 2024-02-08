10639a17e10f289b316e96ad8dfd8576f89d5976
alaukiknpant <alaukikn.pant@gmail.com>
2024-02-08 09:23:24 +0545

    Forge test

--- test-foundry/SampleTest.sol
+++ test-foundry/SampleTest.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import "../forge-std/Test.sol";
+import "../lib/forge-std/src/Test.sol";
 
 contract SampleTest is Test {
   function testSample() public {
