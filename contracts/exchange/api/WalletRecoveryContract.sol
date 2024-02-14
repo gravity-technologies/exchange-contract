c66f899692412f8432d8b0c1e972a7e3b79eff3d
alaukiknpant <alaukikn.pant@gmail.com>
2024-02-14 21:34:30 +0800

    Respond to comments

--- contracts/exchange/api/WalletRecoveryContract.sol
+++ contracts/exchange/api/WalletRecoveryContract.sol
@@ -7,6 +7,8 @@ import "../types/DataStructure.sol";
 import "../util/Address.sol";
 
 contract WalletRecoveryContract is BaseContract {
+  // addRecoveryAddress adds a recoveryAddress for a given signer for a given account
+  // The recoveryAddress can be used to change the signer from the signer to another signer from the account and subAccounts associated with the account
   function addRecoveryAddress(
     int64 timestamp,
     uint64 txID,
@@ -28,6 +30,7 @@ contract WalletRecoveryContract is BaseContract {
     acc.recoveryAddresses[signer][recoveryAddress] = 1;
   }
 
+  // removeRecoveryAddress removes a recoveryAddress for a given signer for a given account
   function removeRecoveryAddress(
     int64 timestamp,
     uint64 txID,
@@ -49,6 +52,11 @@ contract WalletRecoveryContract is BaseContract {
     delete acc.recoveryAddresses[signer][recoveryAddress];
   }
 
+  // recoverAddress replaces the oldSigner with the newSigner with the newSigner having the same permissions
+  // as the oldSigner in the account and all the subAccounts associated with the account.
+  // oldSigner is the existing signer that can have permissions in the account but needs to be replaced
+  // newSigner is the new signer that will replace the oldSigner
+  // recoverySigner is the signer that has to supply the signature to enable the recovery
   function recoverAddress(
     int64 timestamp,
     uint64 txID,
@@ -70,11 +78,14 @@ contract WalletRecoveryContract is BaseContract {
     // ------- End of Signature Verification -------
 
     require(acc.recoveryAddresses[oldSigner][recoverySigner] == 1, "invalid recovery signer");
+    // Add a new signer with the same permission as the old signer to the account
     acc.signers[newSigner] = acc.signers[oldSigner];
     delete acc.signers[oldSigner];
-    for (uint256 i = 0; i < acc.subAccounts.length; i++) {
+    uint256 numSubAccs = acc.subAccounts.length;
+    for (uint256 i = 0; i < numSubAccs; i++) {
       SubAccount storage subAcc = _requireSubAccount(acc.subAccounts[i]);
       require(subAcc.signers[newSigner] == 0, "new signer already exists");
+      // Add a new signer with the same permission as the old signer to the subAccount
       subAcc.signers[newSigner] = subAcc.signers[oldSigner];
       delete subAcc.signers[oldSigner];
     }
