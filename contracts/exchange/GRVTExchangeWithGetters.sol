d8b8079950824657d9f119ec26d348c4e9ad3a66
alaukiknpant <alaukikn.pant@gmail.com>
2024-02-26 19:30:05 +0800

    Fix prettier

--- contracts/exchange/GRVTExchangeWithGetters.sol
+++ contracts/exchange/GRVTExchangeWithGetters.sol
@@ -5,10 +5,4 @@ import "./GRVTExchange.sol";
 import "./api/StateGetter.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-// import "hardhat/console.sol";
-
-// import {BlackScholes as BS} from "./blackscholes/BlackScholes.sol";
-
-contract GRVTExchangeWithGetters is Initializable, GRVTExchange {
-
-}
+contract GRVTExchangeWithGetters is Initializable, GRVTExchange {}
