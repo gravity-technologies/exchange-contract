8e30614d56ff9c5a09df9dee1a37975bc2a30aa0
alaukiknpant <alaukikn.pant@gmail.com>
2024-02-28 18:01:04 +0800

    Test engine

--- test/TestEngine.test.ts
+++ test/TestEngine.test.ts
@@ -55,13 +55,13 @@ function parseTestsFromFile(filePath: string): TestCase[] {
   }
 }
 
-describe("API - TestEngine", function () {
+describe.only("API - TestEngine", function () {
   let contract: Contract
   let snapshotId: string
   var w1 = getDeployerWallet()
   const accID = w1.address
   let ts: number
-  let tests = parseTestsFromFile(process.cwd() + "/test/tests/CreateAccount.json")
+  let tests = parseTestsFromFile(process.cwd() + "/test/tests/TestCreateAccount.json")
 
   before(async () => {
     const deployingWallet = getWallet(LOCAL_RICH_WALLETS[0].privateKey)
@@ -78,28 +78,100 @@ describe("API - TestEngine", function () {
     await network.provider.send("evm_revert", [snapshotId])
   })
 
-  for (var test of tests) {
-    // var test = tests[0]
-    describe(test.name, function () {
-      it("should not revert", async function () {
-        if (test.steps.length === 0) {
-          throw new Error("Test has no steps")
-        }
-        for (const step of test.steps) {
-          var tx: ethers.providers.TransactionRequest = {
-            to: contract.address,
-            gasLimit: 2100000,
-            data: step.tx_data,
-          }
-          w1 = w1.connect(getProvider())
-          const resp = await w1.sendTransaction(tx)
-          if (step.ret != "") {
-            await expectToThrowAsync(resp.wait())
-          } else {
-            await resp.wait()
-          }
-        }
-      })
+  var test = tests[0]
+  describe(test.name, function () {
+    it("should not revert", async function () {
+      await validateTest(test, contract, w1)
     })
-  }
+  })
+
+  var test = tests[1]
+  describe(test.name, function () {
+    it("should not revert", async function () {
+      await validateTest(test, contract, w1)
+    })
+  })
+
+  var test = tests[2]
+  describe(test.name, function () {
+    it("should not revert", async function () {
+      await validateTest(test, contract, w1)
+    })
+  })
+
+  var test = tests[3]
+  describe(test.name, function () {
+    it("should not revert", async function () {
+      await validateTest(test, contract, w1)
+    })
+  })
+
+  var test = tests[4]
+  describe(test.name, function () {
+    it("should not revert", async function () {
+      await validateTest(test, contract, w1)
+    })
+  })
+
+  var test = tests[4]
+  describe(test.name, function () {
+    it("should not revert", async function () {
+      await validateTest(test, contract, w1)
+    })
+  })
+
+  var test = tests[5]
+  describe(test.name, function () {
+    it("should not revert", async function () {
+      await validateTest(test, contract, w1)
+    })
+  })
+
+  var test = tests[6]
+  describe(test.name, function () {
+    it("should not revert", async function () {
+      await validateTest(test, contract, w1)
+    })
+  })
+
+  var test = tests[7]
+  describe(test.name, function () {
+    it("should not revert", async function () {
+      await validateTest(test, contract, w1)
+    })
+  })
+
+  var test = tests[8]
+  describe(test.name, function () {
+    it("should not revert", async function () {
+      await validateTest(test, contract, w1)
+    })
+  })
+
+  var test = tests[9]
+  describe(test.name, function () {
+    it("should not revert", async function () {
+      await validateTest(test, contract, w1)
+    })
+  })
 })
+async function validateTest(test: TestCase, contract: Contract, w1: Wallet) {
+  if (test.steps.length === 0) {
+    throw new Error("Test has no steps")
+  }
+  for (const step of test.steps) {
+    var tx: ethers.providers.TransactionRequest = {
+      to: contract.address,
+      gasLimit: 2100000,
+      data: step.tx_data,
+    }
+    w1 = w1.connect(getProvider())
+    const resp = await w1.sendTransaction(tx)
+    if (step.ret != "") {
+      await expectToThrowAsync(resp.wait())
+    } else {
+      await resp.wait()
+    }
+  }
+  return
+}
