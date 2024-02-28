0065d3ad95aa0864a1921fe7e1bce9f5952defd2
alaukiknpant <alaukikn.pant@gmail.com>
2024-02-28 15:11:53 +0800

    Clean

--- contracts/exchange/GRVTExchangeWithGetters.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
-
-import "./GRVTExchange.sol";
-import "./api/StateGetter.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-
-contract GRVTExchangeWithGetters is Initializable, GRVTExchange {}
