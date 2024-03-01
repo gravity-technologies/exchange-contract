0bf87acab61d344a649d1c0bd758c14e1c2ab607
lowkey_anp <40881096+alaukiknpant@users.noreply.github.com>
2024-03-01 14:07:56 +0800

    Test engine (#71)
    * Test Engine
    
    * Test engine
    
    * Test engine
    
    * Run forge tests last
    
    * Working version
    
    * Works on local
    
    * Add interact script
    
    * Fix tests
    
    * Remove console
    
    * Fix CI
    
    * RUn against in mermory node
    
    * In memory node
    
    * Add logs
    
    * pin era_test_node version to v0.1.0-alpha.11
    
    * alpha15
    
    * Add these tests
    
    * Add tests
    
    * Refactor code
    
    * Refactor again
    
    * Refactor again
    
    * Refactor
    
    * Refactor
    
    * skip in CI
    
    * mark as generated
    
    * Fix typo
    
    * Rename files
    
    * Rename file
    
    ---------
    
    Co-authored-by: Minh Pham <minh@gravitygroup.io>

---
+++ test/engine/TestEngine.test.ts
@@ -0,0 +1,60 @@
+import { Contract, ethers } from "ethers"
+import { Wallet } from "zksync-ethers"
+import { network } from "hardhat"
+import { LOCAL_RICH_WALLETS, deployContract, getWallet } from "../../deploy/utils"
+import { getProvider } from "../../deploy/utils"
+import { expectToThrowAsync, getDeployerWallet, wallet } from "../util"
+import { TestCase, loadTestFilesFromDir, parseTestsFromFile } from "./TestEngineTypes"
+
+const gasLimit = 2100000
+const testDir = "/test/engine/testgen/"
+
+// We skip these tests in CI since the era test node cannot run these tests
+describe.skip("API - TestEngine", function () {
+  let contract: Contract
+  let snapshotId: string
+  let w1 = getDeployerWallet()
+  let testFiles = loadTestFilesFromDir(process.cwd() + testDir)
+
+  before(async () => {
+    const deployingWallet = getWallet(LOCAL_RICH_WALLETS[0].privateKey)
+    contract = await deployContract("GRVTExchange", [], { wallet: deployingWallet, silent: true, noVerify: true })
+  })
+
+  beforeEach(async () => {
+    snapshotId = await network.provider.send("evm_snapshot")
+  })
+
+  afterEach(async () => {
+    await network.provider.send("evm_revert", [snapshotId])
+  })
+
+  testFiles.forEach((file) => {
+    describe(file, async function () {
+      let tests = parseTestsFromFile(process.cwd() + testDir + file)
+      tests.forEach((test) => {
+        it(test.name + ` correctly runs`, async function () {
+          await validateTest(test, contract, w1)
+        })
+      })
+    })
+  })
+})
+
+async function validateTest(test: TestCase, contract: Contract, w1: Wallet) {
+  for (const step of test.steps) {
+    var tx: ethers.providers.TransactionRequest = {
+      to: contract.address,
+      gasLimit: gasLimit,
+      data: step.tx_data,
+    }
+    w1 = w1.connect(getProvider())
+    const resp = await w1.sendTransaction(tx)
+    if (step.ret != "") {
+      await expectToThrowAsync(resp.wait())
+    } else {
+      await resp.wait()
+    }
+  }
+  return
+}
