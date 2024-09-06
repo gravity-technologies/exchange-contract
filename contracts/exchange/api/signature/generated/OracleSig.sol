0c2f442f583febe44ff1a6ea0fddf4f4302196f1
Minh Pham <115865260+minhbsq@users.noreply.github.com>
2024-09-06 16:18:21 +0800

    spearbit-20: optimise gas usage for hashing oracle/trade/liquidation payload (#164)

--- contracts/exchange/api/signature/generated/OracleSig.sol
+++ contracts/exchange/api/signature/generated/OracleSig.sol
@@ -8,14 +8,12 @@ bytes32 constant _ORACLE_H = keccak256("Data(Values[] values,int256 timestamp)Va
 bytes32 constant _ORACLE_VALUE_H = keccak256("Values(int256 sid,int256 v)");
 
 function hashOraclePrice(int64 timestamp, PriceEntry[] calldata prices) pure returns (bytes32) {
-  bytes memory pricesEncoded;
   uint numValues = prices.length;
+  bytes32[] memory hashedPricesElements = new bytes32[](prices.length);
   for (uint i; i < numValues; ++i) {
-    pricesEncoded = abi.encodePacked(
-      pricesEncoded,
-      keccak256(abi.encode(_ORACLE_VALUE_H, prices[i].assetID, prices[i].value))
-    );
+    hashedPricesElements[i] = keccak256(abi.encode(_ORACLE_VALUE_H, prices[i].assetID, prices[i].value));
   }
+  bytes memory pricesEncoded = abi.encodePacked(hashedPricesElements);
   return keccak256(abi.encode(_ORACLE_H, keccak256(pricesEncoded), timestamp));
 }
 
