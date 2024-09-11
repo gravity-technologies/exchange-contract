67fd7d590aecec9d7ff07aacea77ab630978854c
Minh Pham <minh@gravitygroup.io>
2024-09-11 12:47:16 +0800

    spearbit-14: add matched order validation

--- contracts/exchange/types/DataStructure.sol
+++ contracts/exchange/types/DataStructure.sol
@@ -112,10 +112,18 @@ struct State {
   int64 timestamp;
   // Latest Transaction ID
   uint64 lastTxID;
+  // Temporary storage for trade validation. This should always be cleared after each trade
+  mapping(bytes32 => TmpLegData) _tmpTakerLegs;
   // This empty reserved space is put in place to allow future versions to add new
   // variables without shifting down storage in the inheritance chain.
   // See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
   uint256[49] __gap;
+}
+
+struct TmpLegData {
+  bool isBuyingAsset;
+  bool isSet;
+  uint64 limitPrice;
 }
 
 struct Account {
