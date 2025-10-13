d4baca716d00820f70b8021b4f6a2183167f6ba4
Qiu Haoze <haoze@grvt.io>
2025-10-13 14:43:13 +0800

    Update test filter

--- test/engine/RTFEngine.test.ts
+++ test/engine/RTFEngine.test.ts
@@ -10,54 +10,51 @@ import { getTestFixtures, parseTestsFromFile } from "./util";
 const TEST_FIXTURES_DIR = `${process.cwd()}/test/engine/fixtures/`;
 
 describe("API - TestEngine", () => {
-	let exchangeContract: Contract;
-	let l2SharedBridgeAsL1Bridge: L2SharedBridge;
-	let runSnapshotId: string;
-	let testSnapshotId: string;
-	let w1: Wallet;
-	before(async () => {
-		({ exchangeContract, l2SharedBridgeAsL1Bridge, w1 } =
-			await setupTestEnvironment());
-		runSnapshotId = await network.provider.send("evm_snapshot");
-	});
+  let exchangeContract: Contract;
+  let l2SharedBridgeAsL1Bridge: L2SharedBridge;
+  let runSnapshotId: string;
+  let testSnapshotId: string;
+  let w1: Wallet;
+  before(async () => {
+    ({ exchangeContract, l2SharedBridgeAsL1Bridge, w1 } =
+      await setupTestEnvironment());
+    runSnapshotId = await network.provider.send("evm_snapshot");
+  });
 
-	after(async () => {
-		await network.provider.send("evm_revert", [runSnapshotId]);
-	});
+  after(async () => {
+    await network.provider.send("evm_revert", [runSnapshotId]);
+  });
 
-	beforeEach(async () => {
-		testSnapshotId = await network.provider.send("evm_snapshot");
-	});
+  beforeEach(async () => {
+    testSnapshotId = await network.provider.send("evm_snapshot");
+  });
 
-	afterEach(async () => {
-		await network.provider.send("evm_revert", [testSnapshotId]);
-	});
+  afterEach(async () => {
+    await network.provider.send("evm_revert", [testSnapshotId]);
+  });
+  const testFileFilters: string[] = [];
 
-	const testFileFilters = [
-		"TestFundingRateV2_BasicValidation.json",
-		"TestFundingRateV2_NoSpacingCheck.json",
-	];
-	const testFiles = getTestFixtures(TEST_FIXTURES_DIR).filter((name) =>
-		testFileFilters.includes(name),
-	);
-	const testNamesFilter: string[] = [];
+  const testFiles = getTestFixtures(TEST_FIXTURES_DIR).filter((name) =>
+    testFileFilters.includes(name),
+  );
+  const testNamesFilter: string[] = [];
 
-	testFiles.forEach((file) => {
-		describe(file, () => {
-			const tests = parseTestsFromFile(`${TEST_FIXTURES_DIR}/${file}`).filter(
-				(t) => testNamesFilter.length === 0 || testNamesFilter.includes(t.name),
-			);
+  testFiles.forEach((file) => {
+    describe(file, () => {
+      const tests = parseTestsFromFile(`${TEST_FIXTURES_DIR}/${file}`).filter(
+        (t) => testNamesFilter.length === 0 || testNamesFilter.includes(t.name),
+      );
 
-			tests.forEach((test: TestCase) => {
-				it(test.name, async () => {
-					await runTestCase(
-						test,
-						exchangeContract,
-						w1,
-						l2SharedBridgeAsL1Bridge,
-					);
-				});
-			});
-		});
-	});
+      tests.forEach((test: TestCase) => {
+        it(test.name, async () => {
+          await runTestCase(
+            test,
+            exchangeContract,
+            w1,
+            l2SharedBridgeAsL1Bridge,
+          );
+        });
+      });
+    });
+  });
 });
